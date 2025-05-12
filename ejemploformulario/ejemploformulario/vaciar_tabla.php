32dace887b2e589b4c778b16dfb1eafe629bd6a1
GaelRE040414 <leag2004@live.com.mx>
2025-05-12 12:25:43 -0600

    Delete ejemploformulario/ejemploformulario directory
    Copia mal colocada

--- ejemploformulario/ejemploformulario/vaciar_tabla.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conexion = mysqli_connect('localhost','root','','dbejemplo');
-
-    $consulta = "TRUNCATE TABLE historial;";
-    mysqli_query($conexion, $consulta);
-    echo"<script>window.location.href='pagina_historial.php'</script>";
-?>
