bf840511b0b16b85f75eadebc1ac6e2343febea7
GaelRE040414 <leag2004@live.com.mx>
2025-05-21 19:12:12 -0600

    Delete ejemploformulario directory

--- ejemploformulario/vaciar_tabla.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conexion = mysqli_connect('localhost','root','','proyecto_final');
-
-    $consulta = "TRUNCATE TABLE historial;";
-    mysqli_query($conexion, $consulta);
-    echo"<script>window.location.href='pagina_historial.php'</script>";
-?>
